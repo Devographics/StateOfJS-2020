50f1ce89cf217a756aedec367ae7be2227e2b563
SachaG <358832+SachaG@users.noreply.github.com>
2021-01-13 12:04:49 +0900

    Add horizontal scrolling to arrows block on mobile

--- src/core/blocks/tools/ToolsArrowsBlock/ToolsArrowsBlock.tsx
+++ src/core/blocks/tools/ToolsArrowsBlock/ToolsArrowsBlock.tsx
@@ -49,7 +49,7 @@ export const ToolsArrowsBlock = ({ block, data, triggerId = null }: ToolsArrowsB
             }}
             data={data}
         >
-            <ChartContainer fit>
+            <ChartContainer vscroll={false}>
                 <ToolsArrowsChart data={data} current={controlledCurrent} activeCategory="all"/>
             </ChartContainer>
         </Block>
