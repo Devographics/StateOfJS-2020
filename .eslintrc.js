f56579fd0a3a5f16afaae6214d3a71704988bf68
Sacha Greif <SachaG@users.noreply.github.com>
2021-11-09 19:25:53 +0900

    fix eslint issues…?

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,27 @@
 module.exports = {
-  globals: {
-    __PATH_PREFIX__: true,
-  },
-  extends: `react-app`,
-}
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 13,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint"
+    ],
+    "rules": {
+        "react/prop-types": 0
+    }
+};
